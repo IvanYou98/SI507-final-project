170ccefa66e2f20083908b8b0ba330f74e7c910c
IvanYou98 <yuanfanyou@gmail.com>
2022-12-08 00:34:24 -0500

    add comments

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from urllib import response
+"""
+This is the backend server for retrieving data from Yelp Fusion and Google Map API
+"""
 from flask import Flask, request
 import requests
 from cache_utils import *
@@ -6,9 +8,11 @@ from heap_sort import sort
 
 app = Flask(__name__)
 
+# url and api key for google map api
 GOOGLE_MAP_API_HOST = 'https://maps.googleapis.com/maps/api'
 GOOGLE_MAP_API_KEY = 'AIzaSyAQLaMnGgurPe4mLYnr0bNx-8UbxeM9UvM'
 
+# url and api key for yelp fusion api
 YELP_API_KEY = 'KRP7C8GMgshZobRbF4_7Jm3SgZQXUdFDG5O7vz30-dAKyIzVjvHDsGNBPSyxNLgnU70p_iAxB-mO0V417AjEm9DoD-bmxTi_PYT6Yv-drAVCqq7zIQ_1EjM93OlBY3Yx'
 YELP_API_HOST = 'https://api.yelp.com'
 YELP_SEARCH_PATH = '/v3/businesses/search'
@@ -19,6 +23,7 @@ DEFAULT_LOCATION = 'Ann Arbor, MI'
 SEARCH_LIMIT = 3
 
 
+# this is just a testing end point
 @app.route('/default')
 def hello_world():
     url_params = {
@@ -32,31 +37,43 @@ def hello_world():
     return response.json()
 
 
+"""
+Handling the GET request of "/restaurants" end point
+"""
+
+
 @app.route('/restaurants')
 def searchRestaurants():
     term = request.args.get('term')
     location = request.args.get('location')
 
     filename = term + "@" + location + ".json"
+    # if we haven't seen this query before, we will open a cache for this request
     cache_data = open_cache(filename)
     if len(cache_data) == 0:
         url_params = {
             'term': term,
             'location': location
         }
-
+        # concatenate url with two path parameters
         response = requests.request('GET', YELP_API_HOST + YELP_SEARCH_PATH, headers={
             'Authorization': 'Bearer %s' % YELP_API_KEY,
         }, params=url_params)
         data = response.json()
         sort(data['businesses'])
+        # save to cache for future use
         save_to_cache(data, filename)
         return data
+    # if we have seen this query before, we will just return the cached data
     else:
         return cache_data
 
 
-# transform geolocation to address
+"""
+    transform the latitude and longitude to geocode
+"""
+
+
 @app.route('/location/current')
 def getCurrentLocation():
     url_parameter = {
@@ -66,6 +83,11 @@ def getCurrentLocation():
     response = requests.request(
         'GET', GOOGLE_MAP_API_HOST + '/geocode/json', params=url_parameter)
     return response.json()
+
+
+"""
+    transform a location string to geocode
+"""
 
 
 @app.route('/location/geolocation')
--- backend/heap_sort.py
+++ backend/heap_sort.py
@@ -1,18 +1,37 @@
 import json
 from cache_utils import open_cache, save_to_cache
 
+"""
+    sort the input array in descending order by their ratings
+"""
+
 
 def sort(arr):
     build_min_heap(arr)
+    # now we have a min heap, so on the top of our heap is the lowest rated restaurant, we need to swap it with the last
+    # element of array (since we want to sort in descending order), and sink the current top of heap.
     for i in range(len(arr) - 1, -1, -1):
         arr[0], arr[i] = arr[i], arr[0]
         sink(arr, 0, i)
     print(arr)
 
 
+"""
+    build a min heap based on input array
+"""
+
+
 def build_min_heap(arr):
     for i in range(len(arr) // 2 - 1, -1, -1):
         sink(arr, i, len(arr))
+
+
+"""
+sink down the tree node to its correct position
+arr: input array
+root: the index of the tree node in the input array
+end: the right boundary of array (exclusive)
+"""
 
 
 def sink(arr, root, end):
@@ -21,6 +40,8 @@ def sink(arr, root, end):
     left = root * 2 + 1
     right = root * 2 + 2
     smaller = root
+    # if one of the child node has lower rating than the root node, we will swap the lower rated child node
+    # with the root node
     if left < end and arr[left]['rating'] < arr[smaller]['rating']:
         smaller = left
     if right < end and arr[right]['rating'] < arr[smaller]['rating']:
